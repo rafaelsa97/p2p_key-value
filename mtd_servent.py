888f5adcf46bd458ff7a39a0b025ff2c4f7efd99
Rafael Santos de Almeida <rafaelsa97@ufmg.br>
2017-11-30 09:48:32 -0200

    Adicionado método add_lista_chave_valor

--- mtd_servent.py
+++ mtd_servent.py
@@ -3,10 +3,9 @@
 import sys
 import socket
 import struct
-import mtd_servent
 
-# obtem_chave_valor(arquivo_a_ser_lido)
-# Lê um arquivo indicado e obtém a chave e o valor de uma linha
+# obtem_chave_valor(linha_lida_do_arquivo)
+# Obtém a chave e o valor de uma linha lida do arquivo
 # Saída: chave e valor da linha
 def obtem_chave_valor(aux):
 	if aux[0] != '#' and aux != '\n':		# Salva chave contida no arquivo
@@ -20,12 +19,24 @@ def obtem_chave_valor(aux):
 	else:
 		return None, None
 
-def conta_linhas(arquivo):
+# conta_linhas(arquivo_que_contém_as_chaves_e_valores)
+# Conta número de chaves contidas no arquivo
+# Saída: número de chaves
+def conta_chaves(arquivo):
 	c = 0
-	arquivo.seek(0,0)
 	while True:
 		aux = arquivo.readline()
 		if aux == '':	 # Encerra se chegou ao final do arquivo
+			arquivo.seek(0,0)
 			return c
 		elif aux[0] != '#' and aux != '\n':
 			c = c + 1
+
+# add_lista_chave_valor(lista_a_add_chave_e_valor, contador, chave, valor)
+# Adiciona as chaves e valores lidos do arquivo em uma lista
+# Saída: lista com chaves e valores adicionados
+def add_lista_chave_valor(lista,c, chave, valor):
+	lista[c][0] = c
+	lista[c][1] = chave
+	lista[c][2] = valor
+	return lista
--- servent.py
+++ servent.py
@@ -14,12 +14,19 @@ import mtd_servent
 
 nome_arquivo = "teste"
 arquivo = open(nome_arquivo)
+num_chaves = mtd_servent.conta_chaves(arquivo) # Salva o número de chaves contidas no arquivo
+
+lista = [ [ 0 for i in range(3) ] for j in range(num_chaves) ]
+
+c = 0
+
 while True:
 	aux = arquivo.readline()
 	if aux == '':
 		break
 	chave, valor = mtd_servent.obtem_chave_valor(aux)
 	if chave != None and valor != None:
-		print chave + valor
-num_linhas = mtd_servent.conta_linhas(arquivo)
+		lista = mtd_servent.add_lista_chave_valor(lista, c, chave, valor)
+		c = c + 1
+
 arquivo.close()
